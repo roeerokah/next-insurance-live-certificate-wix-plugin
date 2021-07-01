d3f4a68d6d0d9bdc54d5e68df0032995e2f792ce
Roee Rokah <roee.ro@next-insurance.com>
2021-07-01 11:09:01 +0300

    changed start command to recompile and serve the proj removed hour from verify date make all date inside the link reduced the size of the banner

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -42,3 +42,47 @@ $(document).ready(() => {
     console.log('navigated');
   });
 });
+
+// const websiteName = 'Dogiz.com';
+const proofCertificateUrl = "https://portal.nextinsurance.com/certificates/live-certificate/a904576e8d917bddda63d949c15b13ac"
+const verifyCertificateApi = 'https://portal.nextinsurance.com/api/certificates/live-certificate?liveCertificateId=';
+
+window.onload = function() {
+    init();
+    function initElements(){
+        // document.querySelector('#businessName').textContent = websiteName;
+        document.querySelector('#certificateProof').href = proofCertificateUrl;
+    }
+    function getCertificateId() {
+        return proofCertificateUrl.split('live-certificate/')[1];
+    }
+    function getDateTimeNow(){
+        const now = new Date();
+        const offsetMs = now.getTimezoneOffset() * 60 * 1000;
+        const dateLocal = new Date(now.getTime() - offsetMs);
+        const str = dateLocal.toISOString().slice(0, 10).replace(/-/g, "/").replace("T", " ");
+        return str;
+    }
+    function verifyCertificate(){
+        const certificateId = getCertificateId();
+        const verifyCertificateApiFull = `${verifyCertificateApi}${certificateId}`;
+        document.querySelector('#verifiedTimestamp').textContent = getDateTimeNow();
+
+        // fetch(verifyCertificateApiFull)
+        //     .then(data => {
+        //         console.log('data: ', data)
+        //         if (data.status === 200){
+        //             document.querySelector('#verifiedTimestamp').textContent = getDateTimeNow();
+        //             document.querySelector('#banner').setAttribute("class", "ni-banner");
+        //         }else{
+        //             throw new Error('GET_LIVE_CERTIFICATES_NOT_FOUND');
+        //         }
+        //     }).catch(error => {
+        //     console.log('error: ', error);
+        // });
+    }
+    function init() {
+        verifyCertificate();
+        initElements();
+    }
+}
